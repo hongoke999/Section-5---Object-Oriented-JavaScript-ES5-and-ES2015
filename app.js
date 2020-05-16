66da96823c6611027034e437fbbb575b6382c2cd
Chaka Hongoke <hongoke999@yahoo.com>
2020-05-16 09:50:18 +0300

    Sub Classes

--- app.js
+++ app.js
@@ -1,32 +1,28 @@
 class Person {
-  constructor(firstName, lastName, dob) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthday = new Date(dob);
-  }
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 
-  greeting() {
-      return `Hello there ${this.firstName} ${this.lastName}`;
-  }
+    greeting() {
+        return `Hello there ${this.firstName} ${this.lastName}`;
+    }
+}
 
-  calculateAge() {
-      const diff = Date.now() - this.birthday.getTime();
-      const ageDate = new Date(diff);
-      return Math.abs(ageDate.getUTCFullYear() - 1970);
-  }
+class Customer extends Person {
+    constructor(firstName, lastName, phone, membership) {
+        super(firstName, lastName);
 
-  getsMarried(newLastName) {
-      this.lastName = newLastName;
-  }
+        this.phone = phone;
+        this.membership = membership;
+    }
 
-  static addNumbers(x, y) {
-      return x + y;
-  }
-};
+    static getMembershipCost() {
+        return 500;
+    }
+}
 
-const mary = new Person('Mary', 'Williams', '11-13-1980');
+const john = new Customer('John', 'Doe', '555-555-5555', 'Standard');
 
-mary.getsMarried('Thompson');
-console.log(mary);
-
-console.log(Person.addNumbers(5, 8));
+console.log(john.greeting());
+console.log(Customer.getMembershipCost());
